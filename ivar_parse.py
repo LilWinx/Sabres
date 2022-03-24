62296ecfd4ed5b5068191ce4b09abbfa3bf972bc
LilWinx <lil.winx@gmail.com>
2022-03-24 11:06:52 +1100

    clean-up

--- ivar_parse.py
+++ ivar_parse.py
@@ -5,15 +5,18 @@ ignored_columns = ['REGION', 'REF_DP', 'REF_RV', 'REF_QUAL', 'ALT_RV', 'ALT_QUAL
 neworder = ['REF', 'POS', 'ALT', 'REFPOSALT', 'TOTAL_DP', 'ALT_FREQ']
 choices = ('N/A', 'S')
 
-def generate_snpprofile(file, database, outfile):
+def data_setup(file):
+    # tsv file parsing and set up for removal
     data = pd.read_csv(file, sep='\t', header = 0)
-    resistance_markers = pd.read_csv(database, sep='\t', header = 0)
-    pd.set_option('display.max_rows', None)
     df = pd.DataFrame(data)
-    resdf = pd.DataFrame(resistance_markers)
+    pd.set_option('display.max_rows', None)
+    
+    # remove unwanted columns and reorder
     df.drop(ignored_columns, axis = 1, inplace = True)
     df['REFPOSALT'] = df['REF'] + df['POS'].astype(str) + df['ALT']
     df=df.reindex(columns=neworder)
+
+    # calculate synonymous and non-synonymous mutations
     dfsns = pd.DataFrame(data)
     dfsns = dfsns[['POS','REF_AA', 'ALT_AA']] 
     conditions = [
@@ -22,7 +25,23 @@ def generate_snpprofile(file, database, outfile):
     ]
     dfsns['SNS'] = np.select(conditions, choices, default='NS')
     dfmerge = pd.merge(df, dfsns, on='POS', how='left').fillna('-')
-    lineage_merge = pd.merge(dfmerge, resdf, left_on='REFPOSALT', right_on='Mutation', how='left').fillna('-')
-    lineage_merge.drop(['Nucleotide', 'Note'], axis = 1, inplace = True)
-    lineage_merge.to_csv(outfile, sep='\t', index = False)
-    return lineage_merge
+    return dfmerge
+        
+
+def resistance_addition(file, database):
+    # merge the resistance database to the new dataframe 
+    preres_df = pd.DataFrame(data_setup(file))
+    resistance_markers = pd.read_csv(database, sep='\t', header = 0)
+    resdf = pd.DataFrame(resistance_markers)
+    pd.set_option('display.max_rows', None)
+    res_merge = pd.merge(preres_df, resdf, left_on='REFPOSALT', right_on='Mutation', how='left').fillna('-')
+    res_merge.drop(['Nucleotide', 'Note'], axis = 1, inplace = True)
+    return res_merge
+
+def generate_snpprofile(file, database, outfile):
+    # print as separate file for easy manual checking.
+    snpprofile = pd.DataFrame(resistance_addition(file, database))
+    snpprofile.to_csv(outfile, sep='\t', index = False)
+
+    # send to pull_resistance 
+    return snpprofile
