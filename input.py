64335fcb46f7c1c02434d4c917b61d8c496d12d0
LilWinx <lil.winx@gmail.com>
2022-03-24 15:18:44 +1100

    fix for printing error in empty df

--- input.py
+++ input.py
@@ -7,6 +7,9 @@ database = os.path.join(dirname, "database/resistance_markers.txt")
 file = sys.argv[1]
 outfile = os.path.join(os.path.dirname(file), os.path.splitext(os.path.basename(file))[0] + '.snpprofile')
 
-print(pull_resistance.get_resistance_only(file, database, outfile))
+results = pull_resistance.get_resistance_only(file, database, outfile)
+if results:
+    print(results)
+
 
 
