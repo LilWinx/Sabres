365ff5caa67f0c22f9b6b68e2ce448d977095bef
LilWinx <lil.winx@gmail.com>
2022-03-25 17:08:02 +1100

    added argparse, and new test file

--- input.py
+++ input.py
@@ -1,14 +1,25 @@
 import sys
 import os
 import pull_resistance
+import argparse
+
+#file = sys.argv[1]
+
+parser = argparse.ArgumentParser(description='covid_res')
+parser.add_argument('--full', '-f', action='store_true', help='Use Full Database')
+parser.add_argument('input', help='Input file')
+args = vars(parser.parse_args())
 
 dirname = os.path.dirname(__file__)
 database = os.path.join(dirname, "database/resistance_markers.txt")
-file = sys.argv[1]
-outfile = os.path.join(os.path.dirname(file), os.path.splitext(os.path.basename(file))[0] + '.snpprofile')
+full_database = os.path.join(dirname, "database/full_resistance_markers.txt")
+outfile = os.path.join(os.path.dirname(args['input']), os.path.splitext(os.path.basename(args['input']))[0] + '.snpprofile')
 
-results = pull_resistance.get_resistance_only(file, database, outfile)
-if results:
-    print(results)
-
-
+if args['full']:
+	results = pull_resistance.get_resistance_only(args['input'], full_database, outfile)
+	if results:
+		print(results)
+else:
+    results = pull_resistance.get_resistance_only(args['input'], database, outfile)
+    if results:
+        print(results)
