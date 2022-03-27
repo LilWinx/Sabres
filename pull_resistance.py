fa4efcef3b3f0b3e66325aafc9a9d6841fbf1716
LilWinx <lil.winx@gmail.com>
2022-03-28 10:32:47 +1100

    bug fix snpprofile gen

--- pull_resistance.py
+++ pull_resistance.py
@@ -5,7 +5,7 @@ import pandas as pd
 neworder = ['filename', 'REF', 'POS', 'ALT', 'REFPOSALT', 'TOTAL_DP', 'ALT_FREQ', 'REF_AA', 'ALT_AA', 'SNS', 'Protein', 'Mutation', 'Interest', 'Note']
 drop_columns = ['Mutation']
 def get_resistance_only(file, database, outfile):
-    snpprofile_df = pd.DataFrame(ivar_parse.resistance_addition(file, database))
+    snpprofile_df = pd.DataFrame(ivar_parse.generate_snpprofile(file, database, outfile))
     snpprofile_df['filename'] = os.path.splitext(os.path.basename(file))[0]
     snpprofile_df=snpprofile_df.reindex(columns=neworder)
     resistance_df = snpprofile_df[snpprofile_df['Interest'].str.contains('Resistance')]
