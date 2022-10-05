4299424e8dea425afc5da4f7ee5995347b9860ad
Wytamma Wirth <wytamma.wirth@me.com>
2022-10-05 14:15:54 +1100

    :bug: make input required

--- sabres.py
+++ sabres.py
@@ -26,7 +26,7 @@ parser.add_argument(
     help="Specify variant caller software used",
 )
 parser.add_argument("--outdir", "-o", help="Output directory to write to")
-parser.add_argument("--input", "-i", help="Input directory or file")
+parser.add_argument("--input", "-i", help="Input directory or file", required=True)
 parser.add_argument(
     "--version",
     action="version",
