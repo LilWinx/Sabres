f4d2a4662617efa20b65f4b29521a75f6f75f55e
LilWinx <lil.winx@gmail.com>
2023-03-17 15:18:31 +1100

    working bcftools parser

--- sabres/parsers/bcftools_parse.py
+++ sabres/parsers/bcftools_parse.py
@@ -10,8 +10,7 @@ import pandas as pd
 import logging
 
 pd.set_option("display.max_rows", None)
-neworder = ["Filename", "REF", "POS", "ALT", "REFPOSALT", "HET", "DP", "FREQ"]
-
+neworder = ["Filename", "REF", "POS", "ALT", "REFPOSALT", "DP", "FREQ"]
 
 def file_cleanup(file):
     """
@@ -37,7 +36,7 @@ def file2df(file):
     return pd.read_csv(StringIO(file_cleanup(file)), sep="\t", header=0)
 
 
-def varscan_setup(file):
+def bcftools_setup(file):
     """
     converting tsv to dataframe and begin removing unwanted columns
     generates new column called REFPOSALT
@@ -45,29 +44,60 @@ def varscan_setup(file):
     vcf_df = pd.DataFrame(file2df(file))
     if vcf_df.empty:
         return vcf_df
-    vcf_df = vcf_df[["REF", "POS", "ALT", "INFO", "Sample1"]]
-    vcf_df[["adp", "wt", "HET", "hom", "nc"]] = vcf_df.INFO.str.split(";", expand=True)
-    vcf_df[
-        [
-            "GT",
-            "GQ",
-            "SDP",
-            "DP",
-            "RD",
-            "AD",
-            "FREQ",
-            "PVAL",
-            "RBQ",
-            "ABQ",
-            "RDF",
-            "RDR",
-            "ADF",
-            "ADR",
-        ]
-    ] = vcf_df.Sample1.str.split(":", expand=True)
-    vcf_df["REFPOSALT"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"]
-    vcf_df["Filename"] = os.path.splitext(os.path.basename(file))[0]
-    str_rm = "|".join([".varscan.snps"])
-    vcf_df["Filename"] = vcf_df["Filename"].str.replace(str_rm, "")
-    vcf_df = vcf_df.reindex(columns=neworder)
-    return vcf_df
+
+    #masking the INDEL rows
+    #mask = vcf_df.apply(lambda x: x.astype(str).str.contains('INDEL')).any(axis=1)
+    #filtered_df = vcf_df[~mask]
+
+    # split the filtered dataframe by INFO
+    info_data = vcf_df['INFO'].str.split(';', expand=True)
+
+    # masking columns that don't contain DP
+    mask2 = info_data.apply(lambda x: x.astype(str).str.contains('DP', na=False))
+    dp_only_data = info_data[mask2].fillna("")
+    
+    # combining the dp only columns with the filtered dataframe
+    combined_df = pd.concat([vcf_df, dp_only_data], axis=1)
+    combined_df.drop(columns=["INFO"], inplace=True)
+
+    # iterate over the original dataframe and extract the DP and DP4 values
+    dp_df = pd.DataFrame(columns=['DP'])
+    for index, row in combined_df.iterrows():
+        dp_value = ''
+        for col in row:
+            if isinstance(col, str) and col.startswith('DP='):
+                dp_value = col.split('=')[1]
+            elif isinstance(col, str):
+                continue
+            elif isinstance(col, int):
+                continue
+        dp_df.loc[index] = [dp_value]
+
+    dp4_df = pd.DataFrame(columns=['DP4'])
+    for index, row in combined_df.iterrows():
+        dp4_value = ''
+        for col in row:
+            if isinstance(col, str) and col.startswith('DP4='):
+                dp4_value = col.split('=')[1]
+            elif isinstance(col, str):
+                continue
+            elif isinstance(col, int):
+                continue
+        dp4_df.loc[index] = [dp4_value]
+
+    # combine the dp and dp4 columns
+    dp_dp4_df = pd.concat([dp_df, dp4_df], axis=1)
+
+    # calculate the frequency of the alts
+    dp_data = dp_dp4_df['DP4'].str.split(',', expand=True).add_prefix("DP4" + "_")
+    dp_data['alt_sum'] = dp_data['DP4_2'].astype(int) + dp_data['DP4_3'].astype(int)
+    combined_df = pd.concat([combined_df, dp_df, dp_data], axis = 1)
+    combined_df['FREQ'] = combined_df['alt_sum'].astype(int) / combined_df['DP'].astype(int)
+
+    # add refposalt column
+    combined_df["REFPOSALT"] = vcf_df["REF"] + vcf_df["POS"].astype(str) + vcf_df["ALT"]
+
+    # add the file names
+    combined_df["Filename"] = os.path.splitext(os.path.basename(file))[0]
+    combined_df = combined_df.reindex(columns=neworder)
+    return combined_df
