90e58d746559857ae8fcf17cb79751bddfbcd3cc
LilWinx <lil.winx@gmail.com>
2023-03-23 10:57:56 +1100

    update cli tests

--- tests/ext_cli_test.py
+++ tests/ext_cli_test.py
@@ -14,13 +14,13 @@ data_dir = "tests/data/"
         ("fatovcf", os.path.join(data_dir, "fatovcf")),
         ("bcftools", os.path.join(data_dir, "bcftools")),
         ("shiver", os.path.join(data_dir, "shiver")),
-        ("medaka", os.path.join(data_dir, "medaka")),
+        ("medaka", os.path.join(data_dir, "medaka", "testfile_medaka.vcf")),
     ],
 )
 
 def test_cli(vcall, input, out_dir):
     """
-    test_cli_ivar
+    test_cli
     """
     result = subprocess.run(
         [
@@ -39,3 +39,20 @@ def test_cli(vcall, input, out_dir):
     )
 
     assert result.returncode == 0
+    
+    if vcall != "medaka" or vcall != "shiver":
+        expected_files = [
+            "bebtelovimab-res_snpprofile.tab",
+            "molnupiravir-res_snpprofile.tab",
+            "paxlovid-res_snpprofile.tab",
+            "remdesivir-res_snpprofile.tab",
+            "resistant_samples.tab",
+            "sotrovimab-res_snpprofile.tab",
+            "summary_counts.txt",
+        ]
+        
+        for file in expected_files:
+            output_file_path = os.path.join(out_dir, file)
+
+        # check files exists
+        assert os.path.isfile(output_file_path)
